97c67a47a6a9c82d6005dcc7e480f890b8cdcad3
Alex Fernández <alexfernandeznpm@gmail.com>
2017-11-06 19:43:43 +0100

    Deploy to production.

--- start.js
+++ start.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const http = require('http');
+const ssh2 = require('ssh2');
 const childProcess = require('child_process');
 
 function start() {
@@ -12,9 +13,42 @@ function start() {
         return response.end('Crappy because ' + error);
       }
       response.end('Happy at ' + request.url + ': ' + out);
+      connect((error, out) => {
+        if (error) {
+          console.error('Failed at prod: %s', error);
+          return response.end('Crappy at prod because ' + error);
+
+        }
+        response.end('Happily deployed: ' + out);
+      });
     });
   });
   server.listen(8000, () => console.log('Up and happy'));
+}
+
+function connect(callback) {
+  const client = new ssh2.Client();
+  client.on('ready', () => {
+    const buffers = [];
+    client.exec('cd ~/projects/happy/ && git pull', (error, stream) => {
+      if (error) return callback(error);
+      stream.on('close', () => {
+        client.end();
+        return callback(null, Buffer.concat(buffers));
+      });
+      stream.on('data', data => {
+        buffers.push(data);
+      });
+      stream.on('error', error => {
+        buffers.push(error);
+      });
+    });
+  }).connect({
+    host: '54.154.100.15',
+    port: 22,
+    username: 'ubuntu',
+    privateKey: require('fs').readFileSync('../../.ssh/id_rsa'),
+  });
 }
 
 start();
